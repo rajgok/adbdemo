709749a443877b18b05c5557bc54de8517a25f60
rajgok <msgraj.k@gmail.com>
2019-09-11 20:22:04 +0530

    Update: README.md notebooks

--- notebooks/samplenotebook.scala
+++ notebooks/samplenotebook.scala
@@ -29,3 +29,50 @@ spark.conf.set("fs.azure.account.oauth2.client.endpoint." + storageAccountName +
 spark.conf.set("fs.azure.createRemoteFileSystemDuringInitialization", "true")
 dbutils.fs.ls("abfss://" + fileSystemName  + "@" + storageAccountName + ".dfs.core.windows.net/")
 spark.conf.set("fs.azure.createRemoteFileSystemDuringInitialization", "false")
+
+// COMMAND ----------
+
+// MAGIC %sh wget -P /tmp https://raw.githubusercontent.com/Azure/usql/master/Examples/Samples/Data/json/radiowebsite/small_radio_json.json
+
+// COMMAND ----------
+
+dbutils.fs.cp("file:///tmp/small_radio_json.json", "abfss://" + fileSystemName + "@" + storageAccountName + ".dfs.core.windows.net/")
+
+// COMMAND ----------
+
+val df = spark.read.json("abfss://"+ fileSystemName + "@" + storageAccountName + ".dfs.core.windows.net/small_radio_json.json")
+
+
+// COMMAND ----------
+
+val specificColumnsDf = df.select("firstname", "lastname", "gender", "location", "level")
+specificColumnsDf.show()
+
+// COMMAND ----------
+
+val renamedColumnsDF = specificColumnsDf.withColumnRenamed("level", "subscription_type")
+renamedColumnsDF.show()
+
+// COMMAND ----------
+
+val blobStorage = "rakancsa.blob.core.windows.net"
+val blobContainer = "rakancsa"
+val blobAccessKey =  "b02ncGtrKH79UgJUv1C1Kd+hn5Zu7VUKSBK0jGiGSY+LK0d1HEl6xfJ6vGy8aVbxVSzLcdcpdyWomg1wV5ffBQ=="
+
+// COMMAND ----------
+
+val tempDir = "wasbs://" + blobContainer + "@" + blobStorage +"/tempDirs"
+val acntInfo = "fs.azure.account.key."+ blobStorage
+sc.hadoopConfiguration.set(acntInfo, blobAccessKey)
+
+// COMMAND ----------
+
+//SQL Data Warehouse related settings
+val dwDatabase = "rakancdWH"
+val dwServer = "rakanc.database.windows.net"
+val dwUser = "adminlogin"
+val dwPass = "admin@123"
+val dwJdbcPort =  "1433"
+val dwJdbcExtraOptions = "encrypt=true;trustServerCertificate=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30;"
+val sqlDwUrl = "jdbc:sqlserver://" + dwServer + ":" + dwJdbcPort + ";database=" + dwDatabase + ";user=" + dwUser+";password=" + dwPass + ";$dwJdbcExtraOptions"
+val sqlDwUrlSmall = "jdbc:sqlserver://" + dwServer + ":" + dwJdbcPort + ";database=" + dwDatabase + ";user=" + dwUser+";password=" + dwPass
