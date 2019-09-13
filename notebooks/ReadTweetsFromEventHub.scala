3c6158f11203c81d90bd5ead1dfb102640c11a53
rajgok <msgraj.k@gmail.com>
2019-09-13 22:14:46 +0530

    Update: README.md notebooks

---
+++ notebooks/ReadTweetsFromEventHub.scala
@@ -0,0 +1,45 @@
+// Databricks notebook source
+import org.apache.spark.eventhubs._
+    import com.microsoft.azure.eventhubs._
+
+    // Build connection string with the above information
+     val namespaceName = "rakancehns"
+    val eventHubName = "rakanceh"
+    val sasKeyName = "RootManageSharedAccessKey"
+    val sasKey = "DxEPorSUjrEnj/pXvn9TcjryKtgUJyHKZXBh/tCulaI="
+    val connStr = new com.microsoft.azure.eventhubs.ConnectionStringBuilder()
+                .setNamespaceName(namespaceName)
+                .setEventHubName(eventHubName)
+                .setSasKeyName(sasKeyName)
+                .setSasKey(sasKey)
+
+    val customEventhubParameters =
+      EventHubsConf(connStr.toString())
+      .setMaxEventsPerTrigger(5)
+
+    val incomingStream = spark.readStream.format("eventhubs").options(customEventhubParameters.toMap).load()
+
+    incomingStream.printSchema
+
+    // Sending the incoming stream into the console.
+    // Data comes in batches!
+    incomingStream.writeStream.outputMode("append").format("console").option("truncate", false).start().awaitTermination()
+
+// COMMAND ----------
+
+import org.apache.spark.sql.types._
+    import org.apache.spark.sql.functions._
+
+    // Event Hub message format is JSON and contains "body" field
+    // Body is binary, so we cast it to string to see the actual content of the message
+    val messages =
+      incomingStream
+      .withColumn("Offset", $"offset".cast(LongType))
+      .withColumn("Time (readable)", $"enqueuedTime".cast(TimestampType))
+      .withColumn("Timestamp", $"enqueuedTime".cast(LongType))
+      .withColumn("Body", $"body".cast(StringType))
+      .select("Offset", "Time (readable)", "Timestamp", "Body")
+
+    messages.printSchema
+
+    messages.writeStream.outputMode("append").format("console").option("truncate", false).start().awaitTermination()
